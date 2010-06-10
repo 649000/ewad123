1683fc205b3acdb6d828d71e723583f17d5d81d9
ewad123 <ewad123@1d2f26ae-a646-5f9e-f2c0-5cd0e2277016>
2010-06-10 04:12:15 +0000

    my change

--- trunk/P06-3/grails-app/controllers/app/NewController.groovy
+++ trunk/P06-3/grails-app/controllers/app/NewController.groovy
@@ -3,8 +3,7 @@ package app
 class NewController {
 
     def index = {
-        println "Hello Hello"
-        println "CHANGING NEW KOK ANN"
+        println "Hello Hello this is patkoh"
     }
  
 }
